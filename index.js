1568c1ba9567a3fec77024ba2f8812193817ca3f
danibenfica <danielebenfica0@gmail.com>
2023-07-14 12:51:27 -0300

    adicionando media query e README e mudando a data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hora = document.getElementById('hora')
 const minuto = document.getElementById('minuto')
 const segundo = document.getElementById('segundo')
 
-const lancamento = "3 apr 2023"
+const lancamento = "25 dec 2023"
 
 function countDown() {
    const dataLanc = new Date(lancamento)
